19a9b75badbbf35897a3177945508198825ba285
Vlad <mr.6990@mail.ru>
2022-06-25 15:11:10 +0300

    Секция товары + функция гамбургера

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const openBtn = document.querySelector(".hamburger");
+const menu = document.querySelector(".fullscreen-menu");
+const closeMenu = document.querySelector(".fullscreen-menu__close");
+const closeMenuAll = document.querySelector(".menu--vertical");
+
+openBtn.addEventListener("click", e => {
+    menu.classList.add("active");
+});
+
+closeMenu.addEventListener("click", e => {
+    menu.classList.remove("active");
+})
+
+menu.addEventListener("click", e => {
+    if(e.target === menu) {
+        closeMenu.click();
+    } 
+})
