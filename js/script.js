937f33fa3cbcb53050d51d53f0be6805364e0e99
Vlad <mr.6990@mail.ru>
2022-07-06 20:33:58 +0300

    Добавил функционал карты и плеера

--- js/script.js
+++ js/script.js
@@ -215,3 +215,152 @@ $(".app-close-modal").click((e) => {
 
     $.fancybox.close();
 })
+
+//map
+
+let myMap;
+
+const init = () => {
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 12,
+        controls: []
+    });
+
+    const coords = [
+        [55.752004, 37.576133],
+        [55.760686, 37.676911],
+        [55.722314, 37.639396],
+        [55.813083, 37.592057]
+    ];
+
+    const myCollection = new ymaps.GeoObjectCollection({}, {
+        draggable: false,
+        iconLayout: 'default#image',
+        iconImageHref: "./img/icons/marker.svg",
+        iconImageSize: [48, 63],
+        iconImageOffset: [-3, -42]
+    });
+
+    coords.forEach(coord => {
+        myCollection.add(new ymaps.Placemark(coord))
+    })
+
+    myMap.geoObjects.add(myCollection);
+
+    myMap.behaviors.disable('scrollZoom');
+}
+
+ymaps.ready(init);
+
+
+//player 
+
+let player;
+const playerContainer = $('.player');
+
+
+let eventsInit = () => {
+    $(".player__start").click((e) => {
+        e.preventDefault();
+
+        if (playerContainer.hasClass('paused')) {
+            player.pauseVideo()
+        } else {
+            player.playVideo()
+        }
+    });
+
+    $('.player__playback').click((e) => {
+        const bar = $(e.currentTarget);
+        const clickedPosition = e.originalEvent.layerX;
+        const newButtonPositionPercent = (clickedPosition / bar.width()) * 100;
+        const newPlaybackPositionSec = (player.getDuration() / 100) * newButtonPositionPercent;
+
+        $(".player__playback-button").css({
+            left: `${newButtonPositionPercent}%`
+        });
+
+        player.seekTo(newPlaybackPositionSec);
+    })
+    $(".player__splash").click((e) => {
+        player.playVideo();
+    })
+}
+
+const formatTime = timeSec => {
+    const roundTime = Math.round(timeSec);
+
+    const minutes = addZero(Math.floor(roundTime / 60));
+    const seconds = addZero(roundTime - minutes * 60);
+
+    function addZero(num) {
+        return num < 10 ? `0${num}` : num;
+    }
+    
+    return `${minutes} : ${seconds}`;
+}
+
+const onPlayerReady = () => {
+    let interval;
+    const durationSec = player.getDuration();
+    
+    $(".player__duration-estimate").text(formatTime(durationSec));
+
+    if (typeof interval !== 'undefined') {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => {
+        const completedSec = player.getCurrentTime();
+        const completedPercent = (completedSec / durationSec) * 100;
+
+        $(".player__playback-button").css({
+            left: `${completedPercent}%`
+        })
+        $(".player__duration-completed").text(formatTime(completedSec))
+    }, 1000);
+};
+
+const onPlayerStateChange = event => {
+    /* 
+    -1 (воспроизведение видео не начато)
+    0 (воспроизведение видео завершено)
+    1 (воспроизведение)
+    2 (пауза)
+    3 (буферизация)
+    5 (видео подают реплики) 
+    */
+    switch (event.data) {
+        case 1:
+            playerContainer.addClass("active");
+            playerContainer.addClass("paused");
+            break;
+
+        case 2:
+            playerContainer.removeClass("active");
+            playerContainer.removeClass("paused");
+            break;
+    }
+}
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('yt-player', {
+    height: '391',
+    width: '660',
+    videoId: '_gZNClklSEY',
+    events: { 
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange 
+    },
+    playerVars: {
+        controls: 0,
+        disablekb: 0,
+        showinfo: 0,
+        rel: 0,
+        autoplay: 0,
+        modestbranding: 0
+    }
+  });
+}
+
+eventsInit();
